a40eaa57a068755d63fec658622b4b38fe88f6c1
Johna210 <solomonjohna21@gmail.com>
2025-01-01 00:53:18 +0300

    implemented card and product ui

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,16 @@
+import ProductList from "@/components/shared/product/product-list";
+import sampleData from "@/db/sample-data";
+
 const HomePage = async () => {
-  return <>Prostore</>;
+  return (
+    <>
+      <ProductList
+        data={sampleData.products}
+        title="Newest Arrival"
+        limit={4}
+      ></ProductList>
+    </>
+  );
 };
 
 export default HomePage;
