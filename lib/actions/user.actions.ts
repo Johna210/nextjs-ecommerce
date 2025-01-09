510cc261013c87eae334ca7f7d23007da20f361b
Johna210 <solomonjohna21@gmail.com>
2025-01-09 22:35:55 +0300

    Implemented auth

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,8 +1,11 @@
 "use server";
 
-import { signInForSchema } from "../validators";
+import { signInFormSchema, signUpFormSchema } from "../validators";
 import { signIn, signOut } from "@/auth";
 import { isRedirectError } from "next/dist/client/components/redirect-error";
+import { hashSync } from "bcrypt-ts-edge";
+import { prisma } from "@/db/prisma";
+import { formatError } from "../utils";
 
 // Sign in the user with credentials
 export async function signInWithCredentials(
@@ -10,7 +13,7 @@ export async function signInWithCredentials(
   formData: FormData
 ) {
   try {
-    const user = signInForSchema.parse({
+    const user = signInFormSchema.parse({
       email: formData.get("email"),
       password: formData.get("password"),
     });
@@ -29,4 +32,42 @@ export async function signInWithCredentials(
 // Sign out the user
 export async function signOutUser() {
   await signOut();
+}
+
+// Sign up user
+export async function signUpUser(prevState: unknown, formData: FormData) {
+  try {
+    const user = signUpFormSchema.parse({
+      name: formData.get("name"),
+      email: formData.get("email"),
+      password: formData.get("password"),
+      confirmPassword: formData.get("confirmPassword"),
+    });
+
+    const plainPassword = user.password;
+
+    user.password = hashSync(user.password, 10);
+
+    await prisma.user.create({
+      data: {
+        name: user.name,
+        email: user.email,
+        password: user.password,
+        address: {},
+      },
+    });
+
+    await signIn("credentials", {
+      name: user.name,
+      password: plainPassword,
+    });
+
+    return { success: true, message: "User registered successfully" };
+  } catch (error) {
+    if (isRedirectError(error)) {
+      throw error;
+    }
+
+    return { success: false, message: formatError(error) };
+  }
 }
--- lib/constants/index.ts
+++ lib/constants/index.ts
@@ -11,3 +11,10 @@ export const SignInDefaultValues = {
   email: "",
   password: "",
 };
+
+export const SignUpDefaultValues = {
+  name: "",
+  email: "",
+  password: "",
+  confirmPassword: "",
+};
