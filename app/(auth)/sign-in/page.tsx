7d5e338245782577c6985fb7b014ea46ee3c4d24
Johna210 <solomonjohna21@gmail.com>
2025-05-12 15:11:36 +0300

    auth

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,53 +1,56 @@
-// import {
-//   Card,
-//   CardContent,
-//   CardDescription,
-//   CardHeader,
-//   CardTitle,
-// } from "@/components/ui/card";
-// import { APP_NAME } from "@/lib/constants";
-// import Image from "next/image";
-// import Link from "next/link";
-// import CredentialSigninForm from "./credentials-signin-form";
-// import { Metadata } from "next";
-// import { auth } from "@/auth";
-// import { redirect } from "next/navigation";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { APP_NAME } from "@/lib/constants";
+import Image from "next/image";
+import Link from "next/link";
+import CredentialSigninForm from "./credentials-signin-form";
+import { Metadata } from "next";
+import { auth } from "@/auth";
+import { redirect } from "next/navigation";
 
-// export const metadata: Metadata = {
-//   title: "Sign In",
-// };
+export const metadata: Metadata = {
+  title: "Sign In",
+};
 
-const SignInPage = async () => {
-  return <></>;
-  //   const { callbackUrl } = await props.searchParams;
-  //   const session = await auth();
-  //   if (session) {
-  //     return redirect(callbackUrl || "/");
-  //   }
-  //   return (
-  //     <div className="w-full h-screen mx-auto flex items-center justify-center">
-  //       <Card className="w-full max-w-lg px-6">
-  //         <CardHeader className="space-y-4">
-  //           <Link href="/" className="flex-center">
-  //             <Image
-  //               src="/images/logo.svg"
-  //               width={100}
-  //               height={100}
-  //               alt={`${APP_NAME} logo`}
-  //               priority={true}
-  //             />
-  //           </Link>
-  //           <CardTitle className="text-center">Sign In</CardTitle>
-  //           <CardDescription className="text-center">
-  //             Sign in to your account
-  //           </CardDescription>
-  //         </CardHeader>
-  //         <CardContent className="space-y-4">
-  //           <CredentialSigninForm />
-  //         </CardContent>
-  //       </Card>
-  //     </div>
-  //   );
+const SignInPage = async (props: {
+  searchParams: Promise<{
+    callbackUrl: string;
+  }>;
+}) => {
+  const { callbackUrl } = await props.searchParams;
+  const session = await auth();
+  if (session) {
+    return redirect(callbackUrl || "/");
+  }
+  return (
+    <div className="w-full h-screen mx-auto flex items-center justify-center">
+      <Card className="w-full max-w-lg px-6">
+        <CardHeader className="space-y-4">
+          <Link href="/" className="flex-center">
+            <Image
+              src="/images/logo.svg"
+              width={100}
+              height={100}
+              alt={`${APP_NAME} logo`}
+              priority={true}
+            />
+          </Link>
+          <CardTitle className="text-center">Sign In</CardTitle>
+          <CardDescription className="text-center">
+            Sign in to your account
+          </CardDescription>
+        </CardHeader>
+        <CardContent className="space-y-4">
+          <CredentialSigninForm />
+        </CardContent>
+      </Card>
+    </div>
+  );
 };
 
 export default SignInPage;
--- auth.ts
+++ auth.ts
@@ -1,92 +1,92 @@
-// import { compareSync } from "bcrypt-ts-edge";
-// import type { NextAuthConfig } from "next-auth";
-// import NextAuth from "next-auth";
-// import CredentialsProvider from "next-auth/providers/credentials";
+import NextAuth from "next-auth";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { prisma } from "@/db/prisma";
+import CredentialsProvider from "next-auth/providers/credentials";
+import { compareSync } from "bcrypt-ts-edge";
+import type { NextAuthConfig } from "next-auth";
 
-// import { prisma } from "@/db/prisma";
-// import { PrismaAdapter } from "@auth/prisma-adapter";
+export const config = {
+  pages: {
+    signIn: "/sign-in",
+    error: "/sign-in",
+  },
+  secret: process.env.NEXT_AUTH_SECRET,
+  session: {
+    strategy: "jwt",
+    maxAge: 30 * 24 * 60 * 60,
+  },
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    CredentialsProvider({
+      credentials: {
+        email: { type: "email" },
+        password: { type: "password" },
+      },
+      async authorize(credentials) {
+        if (credentials == null) return null;
 
-// export const config = {
-//   pages: {
-//     signIn: "/sign-in",
-//     error: "/sign-in",
-//   },
-//   session: {
-//     strategy: "jwt",
-//     maxAge: 30 * 24 * 60 * 60,
-//   },
-//   adapter: PrismaAdapter(prisma),
-//   providers: [
-//     CredentialsProvider({
-//       credentials: {
-//         email: {
-//           type: "email",
-//         },
-//         password: { type: "password" },
-//       },
-//       async authorize(credentials) {
-//         if (credentials == null) return null;
+        // find the user in the db
+        const user = await prisma.user.findFirst({
+          where: {
+            email: credentials.email as string,
+          },
+        });
 
-//         // Find user in database
-//         const user = await prisma.user.findFirst({
-//           where: {
-//             email: credentials.email as string,
-//           },
-//         });
-//         // Check if user exists and password is correct
-//         if (user && user.password) {
-//           const isMatch = compareSync(
-//             credentials.password as string,
-//             user.password
-//           );
-//           // If password is correct, return user object
-//           if (isMatch) {
-//             return {
-//               id: user.id,
-//               name: user.name,
-//               email: user.email,
-//               role: user.role,
-//             };
-//           }
-//         }
-//         // If user doesn't exist or password is incorrect, return null
-//         return null;
-//       },
-//     }),
-//   ],
-//   callbacks: {
-//     async session({ session, user, trigger, token }: any) {
-//       // Set the user id on the session
-//       session.user.id = token.sub;
-//       session.user.role = token.role;
-//       session.user.name = token.name;
+        if (user && user.password) {
+          const isMatch = compareSync(
+            credentials.password as string,
+            user.password
+          );
 
-//       // If there is an update, set the name on the session
-//       if (trigger === "update") {
-//         session.user.name = user.name;
-//       }
-//       return session;
-//     },
+          if (isMatch) {
+            return {
+              id: user.id,
+              name: user.name,
+              email: user.email,
+              role: user.role,
+            };
+          }
 
-//     async jwt({ token, user, trigger, session }: any) {
-//       // Assign user fields to the token
-//       if (user) {
-//         token.role = user.role;
+          return null;
+        }
+      },
+    }),
+  ],
+  callbacks: {
+    async session({ session, user, trigger, token }: any) {
+      // Set the user ID from the token
+      session.user.id = token.sub;
+      session.user.role = token.role;
+      session.user.name = token.name;
 
-//         // if user has no name then use the email
-//         if (user.name === "NO_NAME") {
-//           token.name = user.email!.split("@")[0];
+      console.log(token);
 
-//           // Update the db to reflect the token name
-//           await prisma.user.update({
-//             where: { id: user.id },
-//             data: { name: token.name },
-//           });
-//         }
-//       }
-//       return token;
-//     },
-//   },
-// } satisfies NextAuthConfig;
+      // If there is an update, set the user name
+      if (trigger === "update") {
+        session.user.name = user.name;
+      }
 
-// export const { handlers, auth, signIn, signOut } = NextAuth(config);
+      return session;
+    },
+    async jwt({ token, user, trigger, session }: any) {
+      // Assign user fields to token
+      if (user) {
+        token.role = user.role;
+
+        // If user has no name then use the email
+        if (user.name === "NO_NAME") {
+          token.name = user.email!.split("@")[0];
+
+          // Update database th reflect the token name
+          await prisma.user.update({
+            where: { id: user.id },
+            data: { name: token.name },
+          });
+        }
+      }
+      return token;
+    },
+  },
+} satisfies NextAuthConfig;
+
+export const { handlers, auth, signIn, signOut } = NextAuth(config);
